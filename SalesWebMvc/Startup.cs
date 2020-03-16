0991e6630f119bbee6505a4866ced3a966ce839f
AlanWilian <awmoliveira@gmail.com>
2020-03-16 23:01:23 +0000

    Sales Record Simple Search and Grouping Search

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -42,6 +42,7 @@ namespace SalesWebMvc
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordsService>();
         }
             
 
